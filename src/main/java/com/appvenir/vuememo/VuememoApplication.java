8a7f498546a57f43637d5022234df0d6b4ccfcbe
Frederic <olivierjp@protonmail.com>
2024-03-22 08:34:17 -0400

    initial commit

---
+++ src/main/java/com/appvenir/vuememo/VuememoApplication.java
@@ -0,0 +1,13 @@
+package com.appvenir.vuememo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuememoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuememoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appvenir/vuememo/VuememoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appvenir.vuememo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuememoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
